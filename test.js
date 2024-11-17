b6f25a8aa02315d18cfe821b1f07f255f74a0dab
Wintermute84 <neuromancerr83@gmail.com>
2024-11-17 13:01:28 +0530

    ddddddddddddddddd

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 async function test() {
   try {
-    let response = await fetch('/home');
+    let response = await fetch('http://localhost:4000/home');
     let data = await response.json();
     console.log(data);
   } catch (error) {
